770b7f41a73437c48bb688fe79c5bae0d7e7cc02
Roshan Dhembare <dhembareroshan777@gmail.com>
2024-11-15 23:48:48 +0530

    form update

--- app.py
+++ app.py
@@ -28,6 +28,28 @@ def success(score):
 def fail(score):
     return "The person has failed and the score is "+ str(score)
 
+
+
+
+#request useful to find whether http request GET or POST
+#render_template use fo
+from flask import render_template,request
+
+#crete form 
+@app.route("/form", methods=['GET','POST'])
+def form():
+    if request.method == 'GET': # display form
+        return render_template("form.html")  #we render to that form we want to open
+    else: #when request is post
+        maths=float(request.form['Maths'])  #we collect all value from form for that we use 'name' field on form
+        english=float(request.form['English'])
+        science=float(request.form['Science'])
+
+        average_marks = (maths + english + science)/3
+
+        return render_template("form.html",score=average_marks)
+
+
 if __name__ == '__main__':
     app.run(debug=True) # wheneven we do some changes we don't need to run server again cause we do debug=True
                 # bydefault it takes two argument one is url but if we don't proovide any url it takes localhost 
