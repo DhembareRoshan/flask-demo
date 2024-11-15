b57889f38ed6670a9c6f0b0b4b0abaacd22f1ffe
Roshan Dhembare <dhembareroshan777@gmail.com>
2024-11-15 20:33:41 +0530

    Add route / and /index

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+##flask app rounting
+
+
+
 from flask import Flask
 
 ## create simple flask application using ojbect name app
 app=Flask(__name__)  ##__name__ is entry point 
+
+#crate route means when we go home page below function trigger
+@app.route("/",methods=["GET"])
+def welcome():
+    return "<h1>Welcome to the channel</h1>"
+
+#create rounte for index function
+@app.route("/index",methods=["GET"])
+def index():
+    return "Welcome to index page"
 
 if __name__ == '__main__':
     app.run(debug=True) # wheneven we do some changes we don't need to run server again cause we do debug=True
