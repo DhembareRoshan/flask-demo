ceb31c9106fce311f099bf873e4837f62312f29b
Roshan Dhembare <dhembareroshan777@gmail.com>
2024-11-15 23:14:43 +0530

    third commit add fail and success route

--- app.py
+++ app.py
@@ -8,14 +8,25 @@ from flask import Flask
 app=Flask(__name__)  ##__name__ is entry point 
 
 #crate route means when we go home page below function trigger
-@app.route("/",methods=["GET"])
+@app.route("/",methods=["GET"])  # GET: Used to retrieve data from the server. It's used for read-only operations.
 def welcome():
-    return "<h1>Welcome to the channel</h1>"
+    return "<h1>Welcome to the channel</h1>"  #here we also use html tag
 
 #create rounte for index function
 @app.route("/index",methods=["GET"])
 def index():
     return "Welcome to index page"
+
+
+# Flast HTTP GET and POST
+# Variable rule if we want to give some value  ... below score is addition parameter
+@app.route("/success/<int:score>")
+def success(score):
+    return "The person has passed and the score is "+ str(score)
+
+@app.route("/fail/<int:score>")
+def fail(score):
+    return "The person has failed and the score is "+ str(score)
 
 if __name__ == '__main__':
     app.run(debug=True) # wheneven we do some changes we don't need to run server again cause we do debug=True
